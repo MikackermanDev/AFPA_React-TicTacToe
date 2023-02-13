67759b189ad3a2784b36aa3ebebd5898ec4b97b8
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-02-13 13:50:44 +0100

    save

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Square() {
+	const [valeur, setValeur] = useState(null);
+	function handleClick() {
+		setValeur("M");
+	}
+	return (
+		<button className="square" onClick={handleClick}>
+			{valeur}
+		</button>
+	);
+}
+
+export default function Board() {
+	return (
+		<>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+		</>
+	);
+}
